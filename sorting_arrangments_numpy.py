9caf5dd82fb95a2ed52aa78ed87eed09735104bf
Tayyab Malik <80326735+tayyabmalik4@users.noreply.github.com>
2021-08-08 10:35:33 +0500

    Update sorting_arrangments_numpy.py
    update

--- sorting_arrangments_numpy.py
+++ sorting_arrangments_numpy.py
@@ -31,3 +31,18 @@ print(arr2)
 print(np.sort(arr2)[::-1])
 # /////this is for 2d array and it reverse the rows and also assending the order
 # print(np.sort(arr1)[::-1])
+
+# //////if we want to know that how places are sorted first than we use argsort function
+print(np.argsort(arr))
+
+# //////numpy is workiong as a pointer if we creat new variable and slicing the previous variable than it is the as a pointer
+# //////and when we change the new variable than automatically change previous variable 
+# /////if we avoid the changes of previous variable than we use copy function like following
+
+# ///with changing arr
+# arr1=arr[3,5]
+# ////without changing arr
+arr1=arr[3,5].copy()
+arr1[0]=9
+print(arr1)
+print(arr)
